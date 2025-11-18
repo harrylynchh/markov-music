cf6a6492c45be8b3e976c67abe9ae7108b2f7645
Nehir Ozden <nehirlaraozden@gmail.com>
2025-11-18 16:05:17 -0500

    worked more on first model

--- first-model.py
+++ first-model.py
@@ -6,7 +6,7 @@
 # particular chords more often, and other chords rarely follow that chords. 
 # We will build our model to find and understand this pattern.
 
-# Okay, here is the plan:
+# General plan:
 # 1. Take corpus of chords
 # 2. Calculate probability distribution for chords to follow a particular chord
 # 3. Define the first chord or make a random choice
@@ -16,3 +16,49 @@
 # 6. …
 # 7. Stochastic music is awesome!
 
+import numpy as np 
+
+# 1. First make the chords into bigrams... why? Making chords into bigrams for 
+# Markov music generation is a way to model the statistical likelihood of one 
+# chord following another, creating more musically plausible progressions than 
+# random selection
+
+# 2. Choose a chord randomly, so need to calc prob of other chords to follow it
+
+# 3. So, calc the freq of each unique bigram to appear in a sequence with that
+# chosen first chord 
+
+# 3. Normalize to get prob distribution of these unique bigrams 
+
+# 4. Move to next state choose randomly from all the options of chords, but all
+# the chords have a more or less likely chance to get chosen (more likely to 
+# choose C cord over G7) 
+# Use np.random.choice(options, p=probabilities) for next choice of chord
+
+
+def predict_next_state(chord:str, data:list=bigrams):
+    """Predict next chord based on current state."""
+    # create list of bigrams which stats with current chord
+    bigrams_with_current_chord = [bigram for bigram in bigrams if bigram.split(' ')[0]==chord]
+    # count appearance of each bigram
+    count_appearance = dict(Counter(bigrams_with_current_chord))
+    # convert apperance into probabilities
+    for ngram in count_appearance.keys():
+        count_appearance[ngram] = count_appearance[ngram]/len(bigrams_with_current_chord)
+    # create list of possible options for the next chord
+    options = [key.split(' ')[1] for key in count_appearance.keys()]
+    # create  list of probability distribution
+    probabilities = list(count_appearance.values())
+    # return random prediction
+    return np.random.choice(options, p=probabilities)
+
+def generate_sequence(chord:str=None, data:list=bigrams, length:int=30):
+    """Generate sequence of defined length."""
+    # create list to store future chords
+    chords = []
+    for n in range(length):
+        # append next chord for the list
+        chords.append(predict_next_state(chord, bigrams))
+        # use last chord in sequence to predict next chord
+        chord = chords[-1]
+    return chords
